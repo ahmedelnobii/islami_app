528cb1cdc7c50dc8e80a0617ea103ffa2faeaf7f
ahmed elnoby <ahmedellnoby13@gmail.com>
2026-07-11 12:57:20 +0300

    add sura widget

---
+++ lib/widgets/tabs_services/quran_service/SuraItemBuilder.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:islami_app/screens/sura_screen.dart';
+import 'package:islami_app/widgets/tabs_services/quran_service/Sura.dart';
+
+class SuraItemBuilder extends StatelessWidget {
+  Sura sura;
+  SuraItemBuilder({required this.sura});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: .opaque,
+      onTap: () {
+        Navigator.of(context).pushNamed(SuraScreen.routeName, arguments: sura);
+      },
+      child: Row(
+        children: [
+          Stack(
+            alignment: .center,
+            children: [
+              SvgPicture.asset(
+                'assets/icon_svg/sura_number_border.svg',
+                fit: .scaleDown,
+              ),
+              Text(
+                sura.sureNumber,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ],
+          ),
+          SizedBox(width: 24),
+          Column(
+            crossAxisAlignment: .start,
+            children: [
+              Text(
+                sura.suraNameEn,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              SizedBox(height: 9),
+              Text(
+                '${sura.suraVersesNumber} Verses',
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+            ],
+          ),
+          Spacer(),
+          Text(sura.suraNameAR, style: Theme.of(context).textTheme.titleLarge),
+        ],
+      ),
+    );
+  }
+}
